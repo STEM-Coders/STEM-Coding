22b736a2047ffa2d007a65e7fe19095dbd6909db
Red_Knight_SBL <Red_Knight_SBL@DESKTOP-ID3GRDJ.attlocal.net>
2020-04-05 19:01:46 -0500

    Added a GameState

---
+++ Corona Trail/src/com/Red_Knight_SBL/Corona_Traill/state/GameState.java
@@ -0,0 +1,24 @@
+package com.Red_Knight_SBL.Corona_Traill.state;
+
+import java.awt.Graphics;
+
+import com.Red_Knight_SBL.Corona_Traill.Handler;
+
+public class GameState extends State{
+
+	public GameState(Handler handler) {
+		super(handler);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void tick() {
+		
+		
+	}
+
+	public void render(Graphics g) {
+		
+		
+	}
+
+}
