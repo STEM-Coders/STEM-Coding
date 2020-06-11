071ed069921612f4960d389351f132953f82f6e7
Red_Knight_SBL <Red_Knight_SBL@DESKTOP-ID3GRDJ.attlocal.net>
2020-06-11 13:31:27 -0500

    Added functionality to MouseManager();.

--- Burger Shootdown Beta/src/game/red_knight_SBL/burgershootdown/input/MouseManager.java
+++ Burger Shootdown Beta/src/game/red_knight_SBL/burgershootdown/input/MouseManager.java
@@ -5,45 +5,78 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 public class MouseManager implements MouseListener, MouseMotionListener {
+	
+	private boolean rightPressed, leftPressed, centerPressed;
+	private int mouseX, mouseY;
+	
+	public MouseManager() {
+		
+	}
+	
+	// Getters
+	
+	public boolean isRightPressed() {
+		return rightPressed;
+	}
+	
+	public boolean isLeftPressed() {
+		return leftPressed;
+	}
+	
+	public boolean isCenterPressed() {
+		return centerPressed;
+	}
+	
+	// Implemented methods
 
 	@Override
-	public void mouseDragged(MouseEvent arg0) {
+	public void mouseDragged(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON1)
+			leftPressed = true;
+		else if (e.getButton() == MouseEvent.BUTTON3)
+			rightPressed = true;
+		else if (e.getButton() == MouseEvent.BUTTON2)
+			centerPressed = true;
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON1)
+			leftPressed = false;
+		else if (e.getButton() == MouseEvent.BUTTON3)
+			rightPressed = false;
+		else if (e.getButton() == MouseEvent.BUTTON2)
+			centerPressed = false;
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		mouseX = e.getX();
+		mouseY = e.getY();
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseMoved(MouseEvent arg0) {
+	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
+	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
+	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
