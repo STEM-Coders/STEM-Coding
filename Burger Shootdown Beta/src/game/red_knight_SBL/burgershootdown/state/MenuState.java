5605d790f7e7fd0b3dfb99b85d6840aa6956a7b5
Bloody Peace Keeper <Bloody Peace Keeper@DESKTOP-STDOTKD.attlocal.net>
2020-12-19 11:25:49 -0600

    Buttons

--- Burger Shootdown Beta/src/game/red_knight_SBL/burgershootdown/state/MenuState.java
+++ Burger Shootdown Beta/src/game/red_knight_SBL/burgershootdown/state/MenuState.java
@@ -1,20 +1,39 @@
 package game.red_knight_SBL.burgershootdown.state;
 
 import java.awt.Graphics;
+import game.red_knight_SBL.burgershootdown.Game;
 import game.red_knight_SBL.burgershootdown.Handler;
 
 public class MenuState extends State{
+	
+	private Game game;
+	private Handler handler;
 	
 	public MenuState(Handler handler) {
 		super(handler);
 		
 	}
 	
-	public void tick() { 
+	public void tick() {
+		
+		button(795, 1068, 345, 430);
 		
 	}
 	
 	public void render(Graphics g) {
+		
+	}
+	
+	private void startGame() {
+		State.setState(game.gameState);
+	}
+	
+	private void button(int xMin, int xMax, int yMin, int yMax) {
+		
+		if (handler.getMouseManager().getMouseX() >= xMin && handler.getMouseManager().getMouseY() >= yMin && handler.getMouseManager().getMouseX() <= xMax && handler.getMouseManager().getMouseY() <= yMax && handler.getMouseManager().isLeftPressed())
+		{
+			
+		}
 		
 	}
 
